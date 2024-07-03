645885b3e9024fd8586879262bed6ea58d0b2e0c
MD.UMAR SALEEM <103065790+Umar9052@users.noreply.github.com>
2024-07-03 16:35:38 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import House from '../assets/house.png'
+
+const Footer = () => {
+  return (
+    <div>
+      <footer>
+        <img src={House} alt = "" />
+      </footer>
+    </div>
+  )
+}
+
+export default Footer
